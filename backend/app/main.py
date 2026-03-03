2e3607a1acf08063aa4029ad41a7ed521b6b4ad2
koushikkumar311 <koushikkumar311@gmail.com>
2026-03-03 22:25:25 +0530

    feat: implement user registration with password hashing

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from app.database import engine, Base
+from app.models import user
+from app.routes.user_routes import router as user_router
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(user_router)
 
 @app.get("/")
 def root():
---
+++ backend/app/routes/user_routes.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.models.user import User
+from app.schemas.user_schema import UserCreate, UserResponse
+from app.core.security import hash_password
+
+router = APIRouter()
+
+@router.post("/register", response_model=UserResponse)
+def register_user(user: UserCreate, db: Session = Depends(get_db)):
+
+    existing_user = db.query(User).filter(User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    new_user = User(
+        email=user.email,
+        hashed_password=hash_password(user.password)
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
